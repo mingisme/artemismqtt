f7615096ad885561f338422add8f6b97e39b99e2
ming.wang4 <ming.wang4@envision-digital.com>
2025-02-13 17:34:02 +0800

    Authentication and Authorization

--- src/main/java/com/example/artemismqtt/MqttBroker.java
+++ src/main/java/com/example/artemismqtt/MqttBroker.java
@@ -1,9 +1,16 @@
 package com.example.artemismqtt;
 
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.security.Role;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
+import org.apache.activemq.artemis.spi.core.security.ActiveMQBasicSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @Configuration
 public class MqttBroker {
@@ -12,9 +19,26 @@ public class MqttBroker {
     public EmbeddedActiveMQ embeddedActiveMQ() throws Exception {
         EmbeddedActiveMQ embeddedActiveMQ = new EmbeddedActiveMQ();
         org.apache.activemq.artemis.core.config.Configuration  config = new ConfigurationImpl();
-        config.setSecurityEnabled(false);
-        config.setPersistenceEnabled(false);
+
+        //authentication
+        config.setSecurityEnabled(true);
+        ActiveMQBasicSecurityManager securityManager = new ActiveMQBasicSecurityManager();
+        Map<String,String> properties = new HashMap<>();
+        properties.put(ActiveMQBasicSecurityManager.BOOTSTRAP_USER, "admin");
+        properties.put(ActiveMQBasicSecurityManager.BOOTSTRAP_PASSWORD, "admin");
+        properties.put(ActiveMQBasicSecurityManager.BOOTSTRAP_ROLE, "amq");
+        securityManager.init(properties);
         config.addAcceptorConfiguration("mqtt", "tcp://0.0.0.0:1083?protocols=MQTT");
+        embeddedActiveMQ.setSecurityManager(securityManager);
+
+        //authorization
+        Role adminRole = new Role("amq", true, true, true, true, true, true, true, true, true, true, true, true);
+        Set<Role> roles = new HashSet<>();
+        roles.add(adminRole);
+        Map<String, Set<Role>> securityRoles = new HashMap<>();
+        securityRoles.put("#", roles);
+        config.setSecurityRoles(securityRoles);
+
         embeddedActiveMQ.setConfiguration(config);
         embeddedActiveMQ.start();
         return embeddedActiveMQ;
