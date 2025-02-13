6f142dba8e7b756d5d6bd021c8d6e6a8cb4d9400
ming.wang4 <ming.wang4@envision-digital.com>
2025-02-13 13:39:57 +0800

    init

---
+++ src/main/java/com/example/artemismqtt/ArtemismqttApplication.java
@@ -0,0 +1,13 @@
+package com.example.artemismqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemismqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemismqttApplication.class, args);
+	}
+
+}
